57bab1f460b37694dbfeb80fa6c4a6d7aafda6ae
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-02-11 16:21:13 +0300

    Выполнить задачи по ТЗ к спринту 7

---
+++ src/ManagerLoadFromFileException.java
@@ -0,0 +1,8 @@
+public class ManagerLoadFromFileException extends RuntimeException {
+        public ManagerLoadFromFileException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+        public ManagerLoadFromFileException(final String message){
+            super(message);
+        }
+    }
